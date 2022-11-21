de56e55d8252931314c1f7cb5f7bb1680ae7a795
bantawa04 <rahunn3@gmail.com>
2022-11-21 18:04:43 +0545

    explecit type insertion

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <p>{{ name }}</p>
+    <button @click="changeName('Zelda')">Change name</button>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from "vue"
+
+  export default defineComponent({
+    name: "App",
+    components: {},
+    data() {
+      return {
+        name: "Link",
+        age: 25 as number | string,
+      }
+    },
+    methods: {
+      changeName(name: string) {
+        this.name = name
+      },
+    },
+  })
+</script>
