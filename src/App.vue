41247620cfe3fef721c30e5ee7852d13da2c0cf2
bantawa04 <rahunn3@gmail.com>
2022-11-22 10:49:48 +0545

    fix:ref with ts

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="app">
-    <p>Name:{{ state.name }}</p>
-    <p>Age: {{ state.age }}</p>
+    <p>Name:{{ name }}</p>
+    <p>Age: {{ age }}</p>
     <button @click="changeName('Zelda')">Change name</button>
     <button @click="changeAge('29')">Change age</button>
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent, reactive } from "vue"
+  import { defineComponent, reactive, ref } from "vue"
 
   export default defineComponent({
     name: "App",
     components: {},
     setup() {
-      const state = reactive({
-        name: "Link",
-        age: 25 as string | number,
-      })
+      // const state = reactive({
+      //   name: "Link",
+      //   age: 25 as string | number,
+      // })
+      const name = ref<string>("Link")
+      const age = ref<string | number>(25)
 
-      function changeName(name: string): void {
-        state.name = name
+      function changeName(n: string): void {
+        name.value = n
+        // this.name = name
       }
-      function changeAge(age: string | number): void {
-        state.age = age
+      function changeAge(a: string | number): void {
+        age.value = a
+        // this.age = age
       }
       return {
-        state,
+        name,
+        age,
         changeName,
         changeAge,
       }
